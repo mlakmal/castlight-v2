0a96100b9cd5bfa7fe6ea53bddd057c96e6faa95
Vikas Reddy <vikasreddy.com@gmail.com>
2017-04-24 17:04:17 +0530

    Custom URL Serializer

---
+++ src/app/castlight.urlserializer.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { UrlTree, UrlSerializer, DefaultUrlSerializer } from '@angular/router';
+
+/*
+  DefaultUrlSerializer serializes the URL after the an AngularJS 1.5 widget
+  serializes it. This confuses the embedded widget.
+
+  Eg:
+    localhost/castlight##find-care?tab=providers
+        gets converted to
+    localhost/castlight##find-care%3Ftab=providers
+      by AngularJS 1.5. This is then converted to
+    localhost/castlight##find-care%243Ftab=providers
+      by Angular
+
+  The fix is convert '%3F' back to '?' before passing it across to Angular.
+  And change '?' to '%3F' while serializing the UrlTree.
+  */
+@Injectable()
+export class CastlightUrlSerializer implements UrlSerializer {
+  private defaultUrlSerializer: DefaultUrlSerializer;
+
+  constructor() {
+    this.defaultUrlSerializer = new DefaultUrlSerializer();
+  }
+
+  parse(url: string): UrlTree {
+    // Put the '?' back
+    let processedUrl: string = url.replace(/%3F/i, '?');
+    return this.defaultUrlSerializer.parse(processedUrl);
+  }
+
+  serialize(tree: UrlTree): string {
+    // Encode '?'
+    return this.defaultUrlSerializer.serialize(tree).replace(/\?/, '%3F');
+  }
+}
